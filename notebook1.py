bcf55bec8aca51734e2fa10fdf1946340dc02f3b
nitratospeed@gmail.com <nitratospeed@gmail.com>
2022-09-19 15:38:32 +0000

    feat: new notebook from dbricks

---
+++ notebook1.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC # hello world!
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ### cmd1
+
+# COMMAND ----------
+
+print('hello')
